55ec9a71cf6dbce9fd56e722142c962f2c0fc634
Ciaran Woodward <cw15g13@soton.ac.uk>
2016-11-30 18:02:01 +0000

    Valid image displayed, not de-interlaced.
    The imagePacker has been modified to correctly create images from incoming
    packets. These are not yet correctly de-interlaced for display.

--- smpteViewerWin/imagePacker.cpp
+++ smpteViewerWin/imagePacker.cpp
@@ -21,37 +21,82 @@ uint8_t * imagePacker::getNextPixels()
 	bool processingRemaining = true;
 	int curSDIDataCount = 0;
 	int ignoreDectets = 0;
+	int syncStart = 0, ancStart = 0;
+	bool horizontalBlanking = 0;
+	bool verticalBlanking = 0;
+	bool skipFirstOctet = false;
+	uint16_t curDectet = 0;
+	uint8_t bitOffset = 0;
 	while (processingRemaining) {
 		const uint8_t * packet = mPacketGetter.getNextPacket();
 		if (packet[43] & 0x80) processingRemaining = false; //Marker is set, final packet of frame
 		int curOffset = 62;
-		uint8_t bitOffset = 0;
 
-		while (curOffset < 1437) {
+		while (curOffset <= 1437) {
 			//Bitwise voodoo to extract the 10bit values from the 16 bit stream
-			uint16_t curDectet = 0;
-			uint16_t mask1 = 0xFF >> bitOffset;
-			uint16_t mask2 = 0xFF << (6 - bitOffset);
-			curDectet += ((uint16_t) packet[curOffset++] & mask1 ) << (6 - bitOffset);
-			curDectet += ((uint16_t) packet[curOffset] & mask2);
+			uint8_t mask1 = 0xFF >> bitOffset;
+			uint8_t mask2 = 0xFF << (6 - bitOffset);
+			if(!skipFirstOctet) curDectet = ((uint16_t)(packet[curOffset++] & mask1)) << (2 + bitOffset);
+			if (curOffset > 1437) {
+				skipFirstOctet = true;
+				continue;
+			}
+			curDectet += ((uint16_t)(packet[curOffset] & mask2)) >> (6-bitOffset);
+			skipFirstOctet = false;
 
 			bitOffset += 2;
-			if (bitOffset == 10) {
+			if (bitOffset == 8) {
 				bitOffset = 0;
 				curOffset++;
+			}
+
+			if (ignoreDectets) {
+				ignoreDectets--;
+				continue;
 			}
 
 			//Remove any special data
 			//TODO: remove TLV data
 			//TODO: Sync to frame starts and edges
-			if (curDectet < 4 || curDectet > 1019) {
-				ignoreDectets = 1;
+			if (curDectet < 4 || curDectet > 1019 || ancStart || syncStart) {
+
+				if (ancStart == 0) {
+					if (curDectet == 0x3FF && syncStart == 0) syncStart = 1;
+					else if (curDectet == 0x00 && syncStart == 1) syncStart = 2;
+					else if (curDectet == 0x00 && syncStart == 2) syncStart = 3;
+					else if (syncStart == 3) {
+						syncStart = 0;
+						if (curDectet & (1 << 7)) {	//V bit = Vertical blanking
+							verticalBlanking = 1;
+						}
+						else {
+							verticalBlanking = 0;
+						}
+
+						if (curDectet & (1 << 6)) {	//H bit = Horizontal blanking
+							horizontalBlanking = 1;
+						}
+						else {
+							horizontalBlanking = 0;
+						}
+					}
+				}
+
+				if (syncStart == 0) {
+					if (curDectet == 0x00 && ancStart == 0) ancStart = 1;
+					else if (curDectet == 0x3FF && ancStart == 1) ancStart = 2;
+					else if (curDectet == 0x3FF && ancStart == 2) ancStart = 3;
+					else if (ancStart == 3 || ancStart == 4) ancStart++;
+					else if (ancStart == 5) {
+						ignoreDectets = (curDectet & 0xFF) + 1;
+						ancStart = 0;
+					}
+				}
+
 				continue;
 			}
-			else if (ignoreDectets) {
-				ignoreDectets--;
-				continue;
-			}
+
+			if (horizontalBlanking) continue;
 
 			//Reduce the range to 8 bits by removing the 2 LSB
 			curDectet = curDectet >> 2;
